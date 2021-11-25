9ee419450c8645ec4c21ed91dcdd7678290c7556
skrevolve <sukyu0919@naver.com>
2021-11-25 17:52:35 +0900

    #3

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+//ormconfig.json 생성 안할시 forRoot()에 선언
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
